7a594fa10779059a1f20da7bded00582ea0009bb
Akshita Rawat <akshitarawat@gmail.com>
2023-06-04 17:04:20 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,13 @@
+class hello
+{ 
+ public void init()
+{ 
+  System.out.println("hello world");
+}
+public static void main[String args()]
+{
+   hello h1=new hello();
+   h1.init();
+   System.out.println("this is the first java prog in git");
+}
+}
