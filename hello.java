d05e571b444a51209db83f3c938994b5efd89f0c
Akshita Rawat <akshitarawat@gmail.com>
2023-06-04 18:53:34 +0530

    hello.java modified sop added to init()

--- hello.java
+++ hello.java
@@ -3,11 +3,14 @@ class hello
  public void init()
 { 
   System.out.println("hello world");
+  
+  System.out.println("Akshita here!");
 }
 public static void main[String args()]
 {
    hello h1=new hello();
    h1.init();
    System.out.println("this is the first java prog in git");
+   
 }
 }
